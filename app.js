92dc8e1ceb8ae0d76f92e1c962dca9221b1cb448
dang-woo <kdw3917ss@naver.com>
2024-03-25 19:59:34 +0900

    restapi-study

--- app.js
+++ app.js
@@ -11,10 +11,31 @@ const goodsRouter = require('./routes/goods.js')
 
 app.use(express.json());
 
-app.get("/",(req, res)=> {
+app.post("/", (req, res) => {
+    console.log(req.body);
+
+    res.send("기본 URI에 POST 메소드가 정상적으로 실행되었습니다.");
+})
+
+app.get("/", (req, res) => {
+    console.log(req.query);
+
+
+    res.status(400).json({
+        "Keykey": "value입니다",
+        "name": "이름일까?",
+    });
+})
+
+app.get("/", (req, res) => {
     console.log(req.query);
 
     res.send('정상적으로 반환되었습니다.')
+})
+
+app.get("/:id", (req, res) => {
+    console.log(req.params);
+    res.send(':id URI에 정상적으로 반환되었습니다')
 })
 
 
@@ -24,7 +45,7 @@ app.get("/",(req, res)=> {
 
 
 // localhost:3000/api -> goodsRouter
-// app.use("/api", [goodsRouter]);
+app.use("/api", [goodsRouter]);
 
 app.listen(port, () => {
     console.log(port, '포트로 서버가 열렸어요!');
